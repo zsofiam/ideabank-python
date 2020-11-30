e2b3fe20c6552efce16d6b5b9051db94c0446684
matyasi.zsofia <zsofia.matyasi@gmail.com>
2020-11-30 18:07:39 +0100

    Add ideas implemented

--- ideabank.py
+++ ideabank.py
@@ -1,1 +1,17 @@
-# Put your code here
+def main():
+    while True:
+        new_idea = input("What is your new idea?")
+        f = open("ideas.txt", "a")
+        f.write("\n" + new_idea)
+        f.close()
+        f = open("ideas.txt", "r")
+        lines = f.readlines()
+        i = 1
+        for line in lines:
+            print(f'{i}. {line}')
+            i += 1
+        f.close()
+    
+
+if __name__ == '__main__':
+    main()
