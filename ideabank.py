6935c120260963c05a6273bf343cdfa84efdfb29
matyasi.zsofia <zsofia.matyasi@gmail.com>
2020-12-01 09:58:17 +0100

    error handling completed and code refactored

--- ideabank.py
+++ ideabank.py
@@ -1,42 +1,54 @@
 import sys
-""" print ("This is the name of the script: ", sys.argv[0])
-print ("Number of arguments: ", len(sys.argv))
-print ("The arguments are: ", str(sys.argv))
-print("Second argument:", sys.argv[1]) """
 
 
 def main():
     if len(sys.argv) == 1:
         while True:
-            new_idea = input("What is your new idea?")
-            f = open("ideas.txt", "a")
-            f.write("\n" + new_idea)
-            f.close()
-            print_lines_with_leading_numbers()
+            ask_for_new_idea()
     elif len(sys.argv) == 2 and sys.argv[1] == "--list":
-        f = open("ideas.txt", "r")
-        lines = f.readlines()
-        i = 1
-        for line in lines:
-            print(f'{i}. {line}')
-            i += 1
-        f.close()
-    elif len(sys.argv) == 3 and sys.argv[1] == "--delete":
-        f = open("ideas.txt", "r")
-        lines2 = f.readlines()
-        print(lines2)
-        f.close()
-        f = open("ideas.txt", "w")
-        f.write('')
-        f.close()
-        lines2.pop(int(sys.argv[2])-1)
-        f = open("ideas.txt", "a")
-        for line in lines2:
-            f.write(line)
-        f.close()
         print_lines_with_leading_numbers()
+    elif len(sys.argv) == 2 and sys.argv[1] == "--delete":
+        print("Specify a number after --delete")
+    elif len(sys.argv) == 3 and sys.argv[1] == "--delete" and sys.argv[2].isnumeric():
+        lines2 = copy_content_into_list()
+        check_if_correct(sys.argv[2], lines2)
+        delete_line_from_file(lines2, sys.argv[2])
+        print_lines_with_leading_numbers()
+    elif len(sys.argv) == 3 and sys.argv[1] == "--delete" and not sys.argv[2].isnumeric():
+        print("Specify a number after --delete")
     else:
         exit()
+
+
+def copy_content_into_list():
+    f = open("ideas.txt", "r")
+    lines2 = f.readlines()
+    f.close()
+    return lines2
+
+
+def delete_line_from_file(lines2, second_arg):
+    f = open("ideas.txt", "w")
+    f.write('')
+    f.close()
+    lines2.pop(int(second_arg)-1)
+    f = open("ideas.txt", "a")
+    for line in lines2:
+        f.write(line)
+    f.close()
+
+
+def check_if_correct(second_arg, lines2):
+    if int(second_arg) >= (len(lines2)-1) or int(second_arg) <= 0:
+        exit()
+
+
+def ask_for_new_idea():
+    new_idea = input("What is your new idea?")
+    f = open("ideas.txt", "a")
+    f.write("\n" + new_idea)
+    f.close()
+    print_lines_with_leading_numbers()
 
 
 def print_lines_with_leading_numbers():
